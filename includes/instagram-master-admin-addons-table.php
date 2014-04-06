21f60710d3a1e949977ed9df257d23ccfc65be97
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-06 23:57:55 +0000

    version 4.3.6
    git-svn-id: https://plugins.svn.wordpress.org/instagram-master/tags/4.3.6@888514 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/instagram-master-admin-addons-table.php
@@ -0,0 +1,66 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class instagram_master_admin_addons_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'instagram_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'instagram_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Installed', 'instagram_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="http://wordpress.techgasp.com/instagram-master/" target="_blank" title="Get Add-ons" style="float:left;">Get Add-ons</a></p></th>
+			<th class="manage-column column-columnname" scope="col"></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="http://wordpress.techgasp.com/instagram-master/" target="_blank" title="Get Add-ons" style="float:right;">Get Add-ons</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-widget-buttons.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Instagram Advanced Buttons Widget</h3><p>The perfect widget to connect your wordpress website to your instagram profile and collect a bunch of photo likes, comments, shares, etc. Packed with the default View on Instagram button and the Sleek Small sized Intagram button, icon.</p><p>The Advanced Instagram Master Buttons Widget looks great when published under the photos widget, remember you can always hide the widget title to get the button closer to your photos.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-yes.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-widget-photos.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Instagram Advanced Photos Widget</h3><p>This is the "Top of the Line" Instagram Photos Display Widget. The Advanced Instagram Master Photos Widget is a Fast clean html5 that makes <b>NO USE</b> of nasty Javascipt or Ajax.</p><p>Beautiful Full Adaptive Photos display in html5 and CSS gives full control over the number of photo thumbnails to display and their sizes. Each thumbnail displays username and tag labels, perfect for Instagram Photo Comments, Love and Share.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-shortcode-in.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Individual Shortcode</h3><p>instagram Master uses TechGasp Wordpress Framework. The <b>Individual Shortcode</b> allows you to have a different customized instagram buttons in each page or post. Easy to use it can be found when you edit a page or a post under the wordpress text editor. Once you have created your shortcode, Just insert the shortcode <b>[instagram-master]</b> anywhere inside that text.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-shortcode-un.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Universal Shortcode</h3><p>instagram Master uses TechGasp Wordpress Framework. The <b>Universal Shortcode</b> allows you to have the same instagram buttons across different pages or posts. Easy to use it can be found right here under the Shortcodes menu. Once you have created your shortcode, Just insert the shortcode <b>[instagram-master-un]</b> anywhere inside the text of your pages or posts.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-updater.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Advanced Updater</h3><p>The Advanced Updater allows you to easily Update / Upgrade your Advanced Plugin. You can instantly update your plugin after we release a new version with more goodies without having to wait for the nightly native wordpress updater that runs every 24/48 hours. Get it fresh, get it fast.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-instagrammaster-admin-addons-support.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="300px" height="139px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Award Winning Professional Support</h3><p>Need professional help deploying this plugin? TechGasp provides 24/7 award winning professional wordpress support for all advanced version costumers and replies to support tickets usually within minutes of being received. Support Us and we will Support You.</p></td>
+			<td class="column-columnname" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-check-no.png', __FILE__); ?>" alt="<?php echo get_option('instagram_master_name'); ?>" align="left" width="200px" height="121px" style="padding:5px;"/></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}
---
+++ includes/instagram-master-widget-buttons.php
@@ -0,0 +1,118 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'instagram_master_widget_buttons' );
+//Register Widget
+function instagram_master_widget_buttons() {
+register_widget( 'instagram_master_widget_buttons' );
+}
+
+class instagram_master_widget_buttons extends WP_Widget {
+	function instagram_master_widget_buttons() {
+	$widget_ops = array( 'classname' => 'Intagram Master Buttons', 'description' => __('Instagram Master Advanced Buttons Widget lets you show 2 sizes of your View on Instagram Button inside any widget position. ', 'instagram_master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'instagram_master_widget_buttons' );
+	$this->WP_Widget( 'instagram_master_widget_buttons', __('Instagram Master Buttons', 'instagram_master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		global $instagram_master_size;
+		extract( $args );
+		//Our variables from the widget settings.
+		$instagram_title = isset( $instance['instagram_title'] ) ? $instance['instagram_title'] :false;
+		$instagram_title_new = isset( $instance['instagram_title_new'] ) ? $instance['instagram_title_new'] :false;
+		$show_instagrambutton = isset( $instance['show_instagrambutton'] ) ? $instance['show_instagrambutton'] :false;
+		$username = $instance['username'];
+		$show_instagrambutton_small = isset( $instance['show_instagrambutton_small'] ) ? $instance['show_instagrambutton_small'] :false;
+		echo $before_widget;
+
+// Display the widget title
+	if ( $instagram_title ){
+		if (empty ($instagram_title_new)){
+		$instagram_title_new = get_option('instagram_master_name');
+		}
+		echo $before_title . $instagram_title_new . $after_title;
+	}
+	else{
+	}
+//Display View on Instagram Button
+	if ( $show_instagrambutton ){
+		if ( $show_instagrambutton_small ){
+			echo '<style>.ig-b- { display: inline-block; }' .
+				'.ig-b- img { visibility: hidden; }' .
+				'.ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }' .
+				'.ig-b-24 { width: 24px; height: 24px; background: url(//badges.instagram.com/static/images/ig-badge-sprite-24.png) no-repeat 0 0; }' .
+				'@media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {' .
+				'.ig-b-24 { background-image: url(//badges.instagram.com/static/images/ig-badge-sprite-24@2x.png); background-size: 60px 178px; } }</style>' .
+				'<a href="http://instagram.com/'.$username.'?ref=badge" class="ig-b- ig-b-24"><img src="//badges.instagram.com/static/images/ig-badge-24.png" alt="Instagram" /></a>';
+		}
+		else{
+		echo '<style>.ig-b- { display: inline-block; }'.
+			'.ig-b- img { visibility: hidden; }'.
+			'.ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }'.
+			'.ig-b-v-24 { width: 137px; height: 24px; background: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24.png) no-repeat 0 0; }'.
+			'@media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {'.
+			'.ig-b-v-24 { background-image: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24@2x.png); background-size: 160px 178px; } }</style>' .
+			'<a href="http://instagram.com/'.$username.'?ref=badge" class="ig-b- ig-b-v-24" target="_blank"><img src="//badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram" /></a>';
+		}
+	}
+	else{
+	}
+
+	echo $after_widget;
+	}
+	//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['instagram_title'] = strip_tags( $new_instance['instagram_title'] );
+		$instance['instagram_title_new'] = $new_instance['instagram_title_new'];
+		$instance['show_instagrambutton'] = $new_instance['show_instagrambutton'];
+		$instance['username'] = $new_instance['username'];
+		$instance['show_instagrambutton_small'] = $new_instance['show_instagrambutton_small'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'instagram_title_new' => __('Instagram Master', 'instagram_master'), 'instagram_title' => true, 'instagram_title_new' => false, 'show_instagrambutton' => false, 'username' => false, 'show_instagrambutton_small' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<br>
+		<b>Check the buttons to be displayed:</b>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['instagram_title'], true ); ?> id="<?php echo $this->get_field_id( 'instagram_title' ); ?>" name="<?php echo $this->get_field_name( 'instagram_title' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'instagram_title' ); ?>"><b><?php _e('Display Widget Title', 'instagram_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'instagram_title_new' ); ?>"><?php _e('Change Title:', 'instagram_master'); ?></label>
+	<br>
+	<input id="<?php echo $this->get_field_id( 'instagram_title_new' ); ?>" name="<?php echo $this->get_field_name( 'instagram_title_new' ); ?>" value="<?php echo $instance['instagram_title_new']; ?>" style="width:auto;" />
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_instagrambutton'], true ); ?> id="<?php echo $this->get_field_id( 'show_instagrambutton' ); ?>" name="<?php echo $this->get_field_name( 'show_instagrambutton' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_instagrambutton' ); ?>"><b><?php _e('View on Instagram Button', 'instagram_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'username' ); ?>"><b><?php _e('Instagram Username:', 'instagram_master'); ?></b></label></br>
+	<input id="<?php echo $this->get_field_id( 'username' ); ?>" name="<?php echo $this->get_field_name( 'username' ); ?>" value="<?php echo $instance['username']; ?>" style="width:auto;" />
+	</p>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_instagrambutton_small'], true ); ?> id="<?php echo $this->get_field_id( 'show_instagrambutton_small' ); ?>" name="<?php echo $this->get_field_name( 'show_instagrambutton_small' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_instagrambutton_small' ); ?>"><b><?php _e('Activate Small Instagram Button', 'instagram_master'); ?></b></label></br>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Instagram Master Website</b>
+	</p>
+	<p><a class="button-secondary" href="http://wordpress.techgasp.com/instagram-master/" target="_blank" title="Instagram Master Info Page">Info Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/instagram-master-documentation/" target="_blank" title="Instagram Master Documentation">Documentation</a> <a class="button-primary" href="http://wordpress.techgasp.com/instagram-master/" target="_blank" title="Visit Website">Get Add-ons</a></p>
+	<?php
+	}
+ }
+?>
